948089cd6a98ca629b9041b9424d1a5bad00b523
Parvez Ahmed <miazyparvez@gmail.com>
2020-12-08 00:30:31 +0600

    fix customer service issue

--- src/app/departments/departments.module.ts
+++ src/app/departments/departments.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+import { DepartmentComponent } from './department/department.component';
 
 
-
+const departmentRoutes: Routes = [
+  {path:'',component:DepartmentComponent}
+];
 @NgModule({
-  declarations: [],
+  declarations: [DepartmentComponent],
   imports: [
     CommonModule
   ]
